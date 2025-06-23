f3fe182a5356724ba0aec3425fe05053d0578963
Segi <segihovav@gmail.com>
2025-06-23 04:45:38 -0700

    Rename event handlers for each button

--- src/components/ModalYesNoDialog.js
+++ src/components/ModalYesNoDialog.js
@@ -21,11 +21,11 @@ export const ModalYesNoDialog = (props) => {
 
                <DialogActions>
                     {typeof props.firstButtonText !== 'undefined' &&
-                         <Button onClick={props.eventHandler} color="primary">{props.firstButtonText}</Button>
+                         <Button onClick={props.firstButtonEventHandler} color="primary">{props.firstButtonText}</Button>
                     }
 
                     {typeof props.secondButtonText !== 'undefined' &&
-                         <Button onClick={props.eventHandler} color="primary">{props.secondButtonText}</Button>
+                         <Button onClick={props.secondButtonEventHandler} color="primary">{props.secondButtonText}</Button>
                     }
                </DialogActions>
           </Dialog>
