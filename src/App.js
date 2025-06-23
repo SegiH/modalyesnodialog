ef68ae54044ddd8c5e85b94c12208fb88bc51cab
Segi <segihovav@gmail.com>
2025-06-23 04:46:32 -0700

    Complete code for demo of ModalYesNoDialog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import ModalYesNoDialog from './components/ModalYesNoDialog';
 import React from 'react';
+import { useState } from "react";
 
 export const App = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const firstButtonEventHandler = () => {
+        alert("OK was clicked");
+        setIsVisible(false);
+    }
+
+    const secondButtonEventHandler = () => {
+        alert("Cancel was clicked");
+        setIsVisible(false);
+    }
+
     return (
         <span>
             <span>ModalYesNoDialog example</span>
 
-            <ModalYesNoDialog isVisible="true" title="test" description="this is a test" />
+            <ModalYesNoDialog isVisible={isVisible} title="test" description="this is a test" firstButtonText="Ok" firstButtonEventHandler={firstButtonEventHandler} secondButtonText="Cancel" secondButtonEventHandler={secondButtonEventHandler} />
         </span>
     )
 }
