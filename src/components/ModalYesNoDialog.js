a0fffacf94ab7c767712213e140dedc136bf6f7a
Segi <segihovav@gmail.com>
2025-06-23 04:19:48 -0700

    Rewrote component as a functional component

--- src/components/ModalYesNoDialog.js
+++ src/components/ModalYesNoDialog.js
@@ -8,37 +8,28 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
-class ModalYesNoDialog extends Component {
-     constructor(props) {
-          super(props);
+const ModalYesNoDialog = (props) => {
+     return (
+          <Dialog id="alert-dialog" open={props.isVisible}>
+               <DialogTitle id="alert-dialog-title"><span>{props.title}</span></DialogTitle>
 
-          this.state = {
-          };      
-     }
-     
-     render() {
-          return (
-               <Dialog id="alert-dialog" open={this.props.isVisible}>
-                    <DialogTitle id="alert-dialog-title"><span>{this.props.title}</span></DialogTitle>
-                  
-                    <DialogContent>
-                         <DialogContentText id="alert-dialog-description">
-                              {this.props.description}
-                         </DialogContentText>
-                    </DialogContent>
-        
-                    <DialogActions>
-                         {typeof this.props.firstButtonText !== 'undefined' &&
-                              <Button onClick={this.props.eventHandler} color="primary">{this.props.firstButtonText}</Button>
-                         }
-                 
-                         {typeof this.props.secondButtonText !== 'undefined' &&
-                              <Button onClick={this.props.eventHandler} color="primary">{this.props.secondButtonText}</Button>
-                         }
-                    </DialogActions>
-               </Dialog>
-          )
-     }
+               <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                         {props.description}
+                    </DialogContentText>
+               </DialogContent>
+
+               <DialogActions>
+                    {typeof props.firstButtonText !== 'undefined' &&
+                         <Button onClick={props.eventHandler} color="primary">{props.firstButtonText}</Button>
+                    }
+
+                    {typeof props.secondButtonText !== 'undefined' &&
+                         <Button onClick={props.eventHandler} color="primary">{props.secondButtonText}</Button>
+                    }
+               </DialogActions>
+          </Dialog>
+     )
 }
 
 // Define the type for all of the props
