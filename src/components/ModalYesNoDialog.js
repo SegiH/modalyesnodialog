03e6e514982a08936b525af73213a3b6c4667ed8
Segi <segihovav@gmail.com>
2025-06-23 04:32:36 -0700

    Remove unused import. Export component

--- src/components/ModalYesNoDialog.js
+++ src/components/ModalYesNoDialog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -8,7 +8,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
-const ModalYesNoDialog = (props) => {
+export const ModalYesNoDialog = (props) => {
      return (
           <Dialog id="alert-dialog" open={props.isVisible}>
                <DialogTitle id="alert-dialog-title"><span>{props.title}</span></DialogTitle>
