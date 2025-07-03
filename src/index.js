5353f4c716242d1a31099a2df60ade4f92b97fa6
Segi <segihovav@gmail.com>
2025-07-02 21:05:25 -0700

    Update all dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { App } from "./App";
 
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+
+// Create a root using React 18+ API (still current in React 19)
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
